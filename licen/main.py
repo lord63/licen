49a9c48e72a09cc82109ed7edd825632167daceb
lord63 <lord63.j@gmail.com>
2015-03-02 13:59:25 +0800

    You can rewrite the year, name and email now.

--- licen/main.py
+++ licen/main.py
@@ -5,13 +5,18 @@
 licen, generates license for you via command line
 
 Usage:
-  licen (ls | list)
-  licen NAME
+  licen (-l | --list)
+  licen LICENSE_NAME
+  licen [-y YEAR] [-f FULLNAME] [-e EMAIL] LICENSE_NAME
   licen (-h | --help)
   licen (-V | --version)
 
 Options:
-  -h --help     Show the helo message.
+  -l --list     List all the support licenses.
+  -y YEAR       Specify the year.
+  -f FULLNAME   Specify the owner's fullname.
+  -e EMAIL      Specify the email.
+  -h --help     Show the help message.
   -V --version  Show the version info.
 
 """
@@ -37,28 +42,37 @@ TEMPLATE_DIR = path.join(ROOT, 'templates')
 LICENSES = sorted(os.walk(TEMPLATE_DIR).next()[2])
 
 
-def get_context():
+def get_default_context():
     year = date.today().year
     fullname = subprocess.check_output(
         'git config --get user.name'.split()).strip()
-    return {'year': year, 'fullname': fullname}
+    email = subprocess.check_output(
+        'git config --get user.email'.split()).strip()
+    return {'year': year, 'fullname': fullname, 'email': email}
 
 
-def handle_licenses(name):
+def handle_licenses(name, user_context):
     if name not in LICENSES:
         raise ValueError("Unsuppport license: {0}".format(name))
     env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
     template = env.get_template(name)
-    context = get_context()
+    context = get_default_context()
+    for key, value in user_context.items():
+        context[key] = value
     return template.render(**context)
 
 
 def main():
     arguments = docopt(__doc__, version=__version__)
-    if arguments['ls'] or arguments['list']:
+    if arguments['--list']:
         print ', '.join(LICENSES)
-    elif arguments['NAME']:
-        print handle_licenses(arguments['NAME'])
+    elif arguments['LICENSE_NAME']:
+        raw_context = {'year': arguments['-y'],
+                       'fullname': arguments['-f'],
+                       'email': arguments['-e']}
+        user_context = {key: value for key, value in raw_context.items()
+                        if value is not None}
+        print handle_licenses(arguments['LICENSE_NAME'], user_context)
     else:
         print __doc__
 
